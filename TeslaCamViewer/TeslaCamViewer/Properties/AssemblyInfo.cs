1a8565e4fe32138b40de6961a68b300501581ffa
Jörg Pichler <joerg.pichler@gmail.com>
2019-10-03 17:08:22 +0200

    Migrate to .NET Core 3.0

--- TeslaCamViewer/TeslaCamViewer/Properties/AssemblyInfo.cs
+++ TeslaCamViewer/TeslaCamViewer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TeslaCam Viewer")]
-[assembly: AssemblyCopyright("Copyright ©  2019 mattw01")]
+[assembly: AssemblyCopyright("Copyright ©  2019 mattw01, Jörg Pichler")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.1.0")]
-[assembly: AssemblyFileVersion("0.4.1.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
